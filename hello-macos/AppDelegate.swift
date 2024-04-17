aaade336f08bc8da8000736c52cb9aa07b207b01
Matthew M. Keeler <mkeeler@launchdarkly.com>
2024-04-17 12:30:02 -0400

    fix: Update to new standard format (#43)

--- hello-macos/AppDelegate.swift
+++ hello-macos/AppDelegate.swift
@@ -1,21 +1,13 @@
-//
-//  AppDelegate.swift
-//  hello-macos
-//
-//  Copyright © 2017 LaunchDarkly. All rights reserved.
-//
-
 import Cocoa
 import LaunchDarkly
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    let launchDarklyMobileKey = ""
+    // Set sdkKey to your LaunchDarkly mobile key.
+    let sdkKey = ""
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-
         setupLDClient()
         if let viewController = NSApp.windows.first?.contentViewController as? ViewController {
             viewController.onApplicationStarted()
@@ -23,24 +15,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
     func setupLDClient() {
-        var contextBuilder = LDContextBuilder(key: "test@email.com")
-        contextBuilder.trySetValue("firstName", .string("Bob"))
-        contextBuilder.trySetValue("lastName", .string("Loblaw"))
-        contextBuilder.trySetValue("groups", .array([.string("beta_testers")]))
+        // Set up the evaluation context. This context should appear on your 
+        // LaunchDarkly contexts dashboard soon after you run the demo.
+        var contextBuilder = LDContextBuilder(key: "example-user-key")
+        contextBuilder.kind("user")
+        contextBuilder.name("Sandy")
 
         guard case .success(let context) = contextBuilder.build()
         else { return }
 
-        var config = LDConfig(mobileKey: launchDarklyMobileKey, autoEnvAttributes: .enabled)
-        config.flagPollingInterval = 30.0
-        config.enableBackgroundUpdates = true
-        config.eventFlushInterval = 30.0
+        var config = LDConfig(mobileKey: sdkKey, autoEnvAttributes: .enabled)
 
         LDClient.start(config: config, context: context)
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
 }
-
